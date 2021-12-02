fa8249d825a0576453595bb2909f7c0fd244ae84
lgchavez <93352618+lgchavez@users.noreply.github.com>
2021-12-02 23:29:39 +0000

    made list

--- todo.py
+++ todo.py
@@ -1,1 +1,15 @@
-
+todo=['new york', 'california','washington state', 'italy', 'france', 'japan', 'brazil', 'spain', 'columbia', 'puerto rico', 'egypt']
+#print(todo)
+#print(len(todo))
+#print(todo.index('columbia'))
+#print("todo")
+#for task in todo:
+ # print(task)
+print('where would you want to travel to?')
+add = input()
+todo.append(add)
+remove = input()
+todo.remove(remove)
+todo.remove('washington state')
+for task in todo:
+  print(task)
