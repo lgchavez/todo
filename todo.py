e9de336bed5bd18ad9cb691ec15d9510e6029a42
lgchavez <93352618+lgchavez@users.noreply.github.com>
2021-12-07 23:22:33 +0000

    added if

--- todo.py
+++ todo.py
@@ -1,15 +1,21 @@
-todo=['new york', 'california','washington state', 'italy', 'france', 'japan', 'brazil', 'spain', 'columbia', 'puerto rico', 'egypt']
+#todo=['new york', 'california','washington state', 'italy', 'france', 'japan', 'brazil', 'spain', 'columbia', 'puerto rico', 'egypt']
+todo=[]
 #print(todo)
 #print(len(todo))
 #print(todo.index('columbia'))
 #print("todo")
 #for task in todo:
  # print(task)
-print('where would you want to travel to?')
-add = input()
-todo.append(add)
-remove = input()
-todo.remove(remove)
-todo.remove('washington state')
-for task in todo:
-  print(task)
+if len(todo)>0:
+  print("what would you like to remove")
+  remove = input()
+  todo.remove(remove)
+  print(todo)
+if len(todo)==0:
+  print("what do you like to do after school")
+  remove = input()
+  todo.append(remove)
+if len(todo)>0:
+  print("what is your favorite sport")
+  remove = input()
+  todo.append(remove)
